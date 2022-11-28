10c81659a90e58e2a5e4f11ecf64ee12849f0063
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 14:07:37 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,14 @@
+"""Programa: Soma
+Objetivo: Ler dois números inteiros, calcular a soma deles e imprimir o resultado na tela. 
+Autora: Tássia Carrozzino 
+Data: 03/11/2022"""
+
+#Entrada
+x = int(input("Digite a primeira parcela: "))
+y = int(input("Digite a segunda parcela: "))
+
+#Processamento
+soma = x+y
+
+#Saída
+print(f"A soma das parcelas {x} e {y} é igual a {soma}.")
