f542aac8228b92041d296fca3eb5dc701eae3703
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 15:27:43 -0300

    Create calculo_passagem.py

---
+++ aula_26-10/calculo_passagem.py
@@ -0,0 +1,13 @@
+"""Programa: Cálculo do valor da passagem. 
+Objetivo: Calcular o valor gasto com passagem de ônibus de um grupo de 20 pessoas, sabendo que o preço unitário da passagem é R$ 7,30.
+Autora: Tássia Carrozzino
+Data: 03/11/2022"""
+
+#Entrada
+valor_unit = float(7.3)
+
+#Processamento
+total = float(20*valor_unit)
+
+#Saída
+print(f"O valor total gasto com passagem para um grupo de 20 usuários é de R$ {total}.")
