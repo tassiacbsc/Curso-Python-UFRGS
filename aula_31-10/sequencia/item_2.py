b75d28e8a82abc5de44b56350989493a081a7d0e
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 21:59:05 -0300

    Create item_2.py

---
+++ aula_31-10/sequencia/item_2.py
@@ -0,0 +1,8 @@
+#Item 2 - Armazene os números 2 e 3, calcule a sua soma e imprima na tela o resultado.
+#Entrada
+num1=2
+num2=3
+#Processamento
+soma1 = num1 + num2
+#Saída
+print(f"O resultado da soma é igual a {soma1}.")
