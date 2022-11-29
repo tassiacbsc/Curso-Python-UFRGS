27d27beaf57da97532a0627633d8afd650887fed
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 21:59:35 -0300

    Create item_3.py

---
+++ aula_31-10/sequencia/item_3.py
@@ -0,0 +1,8 @@
+#Item 3 - Leia dois números do teclado, calcule a sua soma e imprima na tela seu resultado.
+#Entrada
+x = int(input("Digite o primeiro número."))
+y = int(input("Digite o segundo número."))
+#Processamento
+soma_2 = x + y
+#Saída
+print(f"O resultado é igual a {soma_2}.")
