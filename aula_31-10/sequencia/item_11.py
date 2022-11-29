98d6dbdb1d578a9848042a4647693765dd7b00ba
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 22:04:20 -0300

    Create item_11.py

---
+++ aula_31-10/sequencia/item_11.py
@@ -0,0 +1,10 @@
+#Item 11 - Calcule a média aritmética de quatro números fornecidos pelo usuário
+#Entrada
+m1 = int(input("Digite o primeiro valor. "))
+m2 = int(input("Digite o segundo valor. "))
+m3 = int(input("Digite o terceiro valor. "))
+m4 = int(input("Digite o quarto valor. "))
+#Processamento
+media = (m1+m2+m3+m4)/4
+#Saída
+print(media)
