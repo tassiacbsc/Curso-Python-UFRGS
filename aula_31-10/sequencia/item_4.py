4764915fadc4343dbe8285a55409ff8fa2b6ffee
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 22:00:45 -0300

    Create item_4.py

---
+++ aula_31-10/sequencia/item_4.py
@@ -0,0 +1,5 @@
+#Item 4 - Pergunte o nome do usuário e apresente a mensagem ”Oi,...”, seguida pelo nome do usuário
+#Entrada e Processamento
+nome = input("Qual é o seu nome?")
+#Saída
+print(f"Oi, {nome}.")
