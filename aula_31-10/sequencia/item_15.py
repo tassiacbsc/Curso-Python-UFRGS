6725c768e09c899bce987cd469997916dfc1cbf8
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 22:05:15 -0300

    Create item_15.py

---
+++ aula_31-10/sequencia/item_15.py
@@ -0,0 +1,7 @@
+#Item 15 - Leia no teclado 3 números reais e os imprima na tela em ordem crescente.
+#Entrada
+n1 = float(input("\nDigite um número. "))
+n2 = float(input("\nDigite um número. "))
+n3 = float(input("\nDigite um número. "))
+lista = [n1,n2,n3]
+print(sorted(lista))
