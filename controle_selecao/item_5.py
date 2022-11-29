b6ba64f151239cb3bec48f7e3c0067f2e5b1a81a
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 22:09:05 -0300

    Create item_5.py

---
+++ controle_selecao/item_5.py
@@ -0,0 +1,10 @@
+#Item 5 - Leia um número e imprima na tela se ele é negativo, nulo ou positivo.
+
+num = int(input("Digite um número. "))
+if num<0:
+    print("Este número é negativo.")
+elif num==0:
+    print("Este número é nulo. ")
+else:
+    print("Este número é positivo.")
+    
