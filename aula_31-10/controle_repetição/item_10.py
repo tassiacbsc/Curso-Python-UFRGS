ff1bf168e39972ad78ffe5133f781effd81c9bcc
tassiacbsc <117387606+tassiacbsc@users.noreply.github.com>
2022-11-28 21:56:41 -0300

    Create item_10.py

---
+++ aula_31-10/controle_repetição/item_10.py
@@ -0,0 +1,5 @@
+#Item 10 - leia dois números a e b e calcule a potência a**b
+a = float(input("Digite um número. "))
+b = float(input("Digite um número. "))
+potencia = a**b
+print(f"A potência é igual a {potencia}.")
